07615b41157670c22e851b51fe56ae7f1f88d41b
andripurnama <andri.purnama995@hotmail.com>
2024-03-31 17:01:45 +0700

    test: added jest config for testing purpose

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@exmpl/(.*)': '<rootDir>/src/$1'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json']
+};
